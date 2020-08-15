2a1037f1f538e0aa2b05212bf1dd4d7d9f2b2504
jrmylee <jrmylee@berkeley.edu>
2020-08-15 01:06:17 -0700

    encode labels

--- models/dataset.py
+++ models/dataset.py
@@ -19,5 +19,5 @@ class ChordDataset(Dataset):
         file = torch.load(os.path.join(self.main_dir, filename))
 
         audio = torch.log(torch.abs(file['feature']) + 1e-6)
-        sample = { 'audio': audio, 'chord': file['chord']}
+        sample = { 'audio': audio, 'chord': le.fit_transform(file['chord'])}
         return sample
