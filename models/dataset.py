145ab433d5af6fcc36fc08ae91b0db33bc46c15e
jrmylee <jrmylee@berkeley.edu>
2020-08-17 15:33:51 -0700

    chord class

--- models/dataset.py
+++ models/dataset.py
@@ -1,12 +1,12 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
-from sklearn.preprocessing import LabelEncoder
+from models.chords import *
 import numpy as np
 
 import os
 class ChordDataset(Dataset):
     def __init__(self, main_dir, paths):
-        self.le = LabelEncoder()
+        self.chord_class = Chords()
         self.main_dir = main_dir
         self.paths = paths
     def __len__(self):
@@ -18,6 +18,11 @@ class ChordDataset(Dataset):
         filename = self.paths[idx]
         file = torch.load(os.path.join(self.main_dir, filename))
 
+        chords = self.chord_class.chords(file['chords'])
+        chords = self.chord_class.reduce_to_triads(chords)
+        chords = self.chord_class.assign_chord_id(chords)
+        chords = list(chords['chord_id'])
+
         audio = torch.log(torch.abs(file['feature']) + 1e-6)
-        sample = { 'audio': audio, 'chords': self.le.fit_transform(file['chords'])}
+        sample = { 'audio': audio, 'chords': chords}
         return sample
